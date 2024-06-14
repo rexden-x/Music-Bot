d6a9f5d37ae44d1d22892ecf3147abfb5e6f6344
Rexden X <156419201+rexden-x@users.noreply.github.com>
2024-06-13 23:25:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -129,7 +129,7 @@ app.get('/', (req, res) => {
   res.sendFile(imagePath);
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to GlaceYT: http://localhost:${port}`);
+  console.log(`🔗 Listening to Rexden: http://localhost:${port}`);
 });
 printWatermark();
 
